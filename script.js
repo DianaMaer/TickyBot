887c10e934643edb55a25a4ca016b53474e2b838
Diana <DianaMaer@users.noreply.github.com>
2020-07-27 10:35:31 +0200

    create the scroll effectto the corresponding section that was clicked from the navbar

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const smoothScroll = (target, duration) => {
-   let targetElem= document.querySelector(target);
-    let targetPosition =  targetElem.getBoundingClientRect().top;
-    let startPosition = window.pageYOffset;
-    let distance = targetPosition - startPosition;
-    let startTime = null;
-    console.log(startPosition);
+// trying to create create the sooth scroll effect with a tutotial;
+// const smoothScroll = (target, duration) => {
+//    let targetElem= document.querySelector(target);
+//     let targetPosition =  targetElem.getBoundingClientRect().top;
+//     let startPosition = window.pageYOffset;
+//     let distance = targetPosition - startPosition;
+//     let startTime = null;
+//     console.log(startPosition);
+// }
+
+// smoothScroll('#features', 1000);
+
+
+// create the sooth scroll effect;
+const scrollToView = (idname) => {
+  document.getElementById(idname).scrollIntoView({behavior: 'smooth',     });
 }
 
 
-smoothScroll('#features', 1000);
